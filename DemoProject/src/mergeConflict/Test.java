fdd3cccb0ed397bb6c0455f6488808353c59bbee
sreelakshmi665 <16691a0589@mits.ac.in>
2021-02-04 17:24:34 +0530

    merge conflict araises

--- DemoProject/src/mergeConflict/Test.java
+++ DemoProject/src/mergeConflict/Test.java
@@ -3,8 +3,8 @@ package mergeConflict;
 public class Test {
 public static void main(String[] args) {
 	System.out.println("Hello!");
-	int x=10;
-	int y=20;
-	System.out.println(x+y);
+	int number1=10;
+	int number2=20;
+	System.out.println(number1+number2);
 }
 }
