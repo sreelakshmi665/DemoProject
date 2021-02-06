fe1ff5740412c20de67b7019a1b152cf5c2b798f
sreelakshmi665 <16691a0589@mits.ac.in>
2021-02-06 10:43:00 +0530

    commit

--- DemoProject/src/mergeConflict/Test.java
+++ DemoProject/src/mergeConflict/Test.java
@@ -3,12 +3,16 @@ package mergeConflict;
 public class Test {
 public static void main(String[] args) {
 	System.out.println("Hello!");
+	System.out.println("Enter 2 numbers");
 	int number1=10;
 	int number2=20;
 	System.out.println(number1+number2);
+	System.out.println("enter 2 numbers");
 	int data1=10;
 	int data2=20;
+	System.out.println("Hello!");
+	System.out.println("Hello!");
 	System.out.println(data1*data2);
-	
+
 }
 }
